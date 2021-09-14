a143205b6ac1e787223e45a99f429e02a0f9a620
Kacper Stolarek <Capure@users.noreply.github.com>
2021-09-14 09:42:19 +0200

    :bug: Fixed the previous fix

--- src/components/home/minimalistic/DayPicker.vue
+++ src/components/home/minimalistic/DayPicker.vue
@@ -32,14 +32,14 @@ export default defineComponent({
     const currentDate = ref(new Date().toISOString().split("T")[0]);
 
     const setPrev = () => {
-      if (currentIdx.value === 6 || currentIdx.value === 0) {
+      if (currentIdx.value === 0) {
         currentOffset.value = currentOffset.value - 7;
       }
             currentIdx.value = currentIdx.value === 0 ? 6 : currentIdx.value - 1;
     };
 
     const setNext = () => {
-      if (currentIdx.value === 0 || currentIdx.value === 6) {
+      if (currentIdx.value === 6) {
         currentOffset.value = currentOffset.value + 7;
       }
             currentIdx.value = currentIdx.value === 6 ? 0 : currentIdx.value + 1;
