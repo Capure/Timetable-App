1fd9ff2a3d7dfeb501463f145bfef7bd53fb8cd5
Kacper Stolarek <Capure@users.noreply.github.com>
2022-09-02 07:37:32 +0200

    :sparkles: Switched to the new api

--- src/api/index.ts
+++ src/api/index.ts
@@ -34,7 +34,7 @@ export const filterLessons = (
 };
 
 export const getLessons = async (weekStart: number): Promise<TimetableData> => {
-  const rawRes = await fetch("https://timetable-api.up.railway.app/graphql", {
+  const rawRes = await fetch("http://api.vlo.software/graphql", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
