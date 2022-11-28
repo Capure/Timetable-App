4dc3fa7d661694536f9aa6215bfbcf3f4f423683
Kacper Stolarek <59137941+Capure@users.noreply.github.com>
2022-11-28 11:29:14 +0100

    :bug: Disabled an iOS hack

--- src/App.vue
+++ src/App.vue
@@ -22,34 +22,7 @@ export default defineComponent({
     const settings = reactive({ ...defaultSettings });
 
     const setBodyBackgroundColor = () => {
-      if (
-        [
-          "iPad Simulator",
-          "iPhone Simulator",
-          "iPod Simulator",
-          "iPad",
-          "iPhone",
-          "iPod",
-        ].includes(navigator.platform)
-      ) {
-        // https://stackoverflow.com/questions/12043187/how-to-check-if-hex-color-is-too-black
-        const c = settings.mainColor.substring(1); // strip #
-        const rgb = parseInt(c, 16); // convert rrggbb to decimal
-        const r = (rgb >> 16) & 0xff; // extract red
-        const g = (rgb >> 8) & 0xff; // extract green
-        const b = (rgb >> 0) & 0xff; // extract blue
-
-        const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
-        if (luma > 128) {
-          document.body.style.backgroundColor = `#${Math.round(r / 2).toString(
-            16
-          )}${Math.round(g / 2).toString(16)}${Math.round(b / 2).toString(16)}`;
-        } else {
-          document.body.style.backgroundColor = settings.mainColor;
-        }
-      } else {
         document.body.style.backgroundColor = settings.mainColor;
-      }
     };
 
     const setSettings = (newSettings: Settings) => {
